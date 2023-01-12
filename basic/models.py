68a2827fd079c8c1cbcafa77a27da5f941f03734
hitesh <hiteshgour@wewinlimited.in>
2023-01-12 17:58:25 +0530

    Create Resume project

---
+++ basic/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+
+
+class BasicInfo(models.Model):
+    pass
+
+class Summery(models.Model):
+    pass
+
+class Experence(models.Model):
+    pass
+
+
+class Education(models.Model):
+    pass
+
+
+class Certifications(models.Model):
+    pass
+
+class Skills(models.Model):
+    pass
+
+class Hobbies(models.Model):
+    pass
+
+class Languages(models.Model):
+    pass
